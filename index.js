0e03076bde3d5f1f3661bcdbc17bf2f4f306b487
KOPIL UDDIN <kopil7132@gmail.com>
2026-05-21 23:31:53 +0600

    Setup

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express')
 const dontenv = require('dotenv')
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dontenv.config()
 
 const uri = process.env.MONGODB_URI;
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const PORT = process.env.PORT
 const client = new MongoClient(uri, {
@@ -19,14 +22,28 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+        const db = client.db("docappoint")
+        const docappointCollection = db.collection("docappoint")
+
+        app.get('/all-appointments', async (req , res) => {
+            const result = await docappointCollection.find().toArray()
+            res.json(result)
+        })
+
+        app.post('/destination' , async(req , res) => {
+            const destination = req.body
+            console.log(destination);
+           const result = await docappointCollection.insertOne(destination)
+           res.json(result)
+        })
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);

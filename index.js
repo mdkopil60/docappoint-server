c78c6c001a52d39c41fa4ad16862634249f60080
KOPIL UDDIN <kopil7132@gmail.com>
2026-05-22 21:11:19 +0600

    post api done

--- index.js
+++ index.js
@@ -32,36 +32,26 @@ async function run() {
             res.json(result)
         })
         app.get('/all-appointments/:id', async (req, res) => {
-
             try {
-
                 const id = req.params.id;
-
                 // Check valid ObjectId
                 if (!ObjectId.isValid(id)) {
                     return res.status(400).json({
                         message: "Invalid ID"
                     });
                 }
-
                 const query = {
                     _id: new ObjectId(id)
                 };
-
                 const result = await docappointCollection.findOne(query);
-
                 if (!result) {
                     return res.status(404).json({
                         message: "Doctor not found"
                     });
                 }
-
                 res.json(result);
-
             } catch (error) {
-
                 console.log(error);
-
                 res.status(500).json({
                     message: "Server Error"
                 });
